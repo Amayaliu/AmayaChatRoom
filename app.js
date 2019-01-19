f667085df23127f258e709f69ce243589c7af197
Amaya_liu <1519357095@qq.com>
2019-01-19 15:45:17 +0800

    node开发的基础聊天室，改良中~~~

---
+++ app.js
@@ -0,0 +1,77 @@
+/*
+ * @Author: Amaya_liu 
+ * @Date: 2019-01-18 10:48:37 
+ * @Last Modified by: Amaya_liu
+ * @Last Modified time: 2019-01-18 11:47:32
+ */
+
+var ws = require('nodejs-websocket');
+ 
+var server = ws.createServer(function(conn) {
+ 
+    conn.on('text', function(str) {
+ 
+        var data = JSON.parse(str);
+        console.log(data);
+ 
+        switch (data.type) {
+            case 'setname':
+                conn.nickname = data.name;
+                
+                boardcast(JSON.stringify({
+                    type: 'serverInformation',
+                    message: data.name + ' 加入房间'
+                }));
+ 
+                boardcast(JSON.stringify({
+                    type: 'chatterList',
+                    list: getAllChatter()
+                }))
+                break;
+        
+            case 'chat':
+                boardcast(JSON.stringify({
+                    type: 'chat',
+                    name: conn.nickname,
+                    message: data.message
+                }));
+                break;
+ 
+            default:
+                break;
+        }
+    });
+ 
+    conn.on('close', function() {
+        boardcast(JSON.stringify({
+            type: 'serverInformation',
+            message: conn.nickname + ' 离开房间'
+        }));
+ 
+        boardcast(JSON.stringify({
+            type: 'chatterList',
+            list: getAllChatter()
+        }))
+    });
+ 
+    conn.on('error', function(err) {
+        console.log(err);
+    });
+}).listen(4399);
+ 
+function boardcast(str) {
+    server.connections.forEach(function(conn) {
+        conn.sendText(str);
+    })
+}
+ 
+function getAllChatter() {
+ 
+    var chatterArr = [];
+ 
+    server.connections.forEach(function(conn) {
+        chatterArr.push({name: conn.nickname});
+    })
+ 
+    return JSON.stringify(chatterArr);
+}
